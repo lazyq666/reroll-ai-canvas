64ef60bfd0801c972e63c3fc385ca4a8f8794dd1
lazyq666 <119386317+lazyq666@users.noreply.github.com>
2026-09-20 18:17:41 +0800

    fix: refresh i18n cache fingerprints

--- static/js/i18n.js
+++ static/js/i18n.js
@@ -1,5 +1,5 @@
 (function(){
-    const VERSION = 'i18n-609ea7a11888';
+    const VERSION = 'i18n-bdcde84d071f';
     const scripts = [
         '/static/js/i18n-core.js',
         '/static/js/i18n/common.js',
